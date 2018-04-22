6d2e1e5dff1949aff7d50fa2d3076648f71e2ff4
NickLatkovich <n.latkovich@pixelplex.by>
2018-04-22 10:52:44 +0300

    ...

--- test/Main.js
+++ test/Main.js
@@ -6,25 +6,19 @@ const { users } = config.test;
 const session = new Session();
 
 describe('connection to graphigram', () => {
-	after(() => {
-		session.close();
-	});
 	it('successful', (done) => {
-		session.signUp(users[0].username, users[0].postingKey, 'qweasd').catch((err) => {
-			throw err;
-		}).then(() => done());
+		session.signUp(users[0].username, users[0].postingKey, 'qweasd')
+			.then(() => done())
+			.catch(done);
 	}).timeout(15000);
 	it('hasConnection', (done) => {
-		session.hasConnectionWithUser(users[1].username)
-			.then(console.log)
+		session.isConnectionExists(users[0].username, users[1].username)
 			.then(() => done())
-			.catch((err) => {
-				throw err;
-			});
+			.catch(done);
 	}).timeout(15000);
 	it('channel creation', (done) => {
 		session.createChannel(users[1].username)
-			.then(console.log)
-			.then(() => done());
+			.then(() => done())
+			.catch(done);
 	})
 });
